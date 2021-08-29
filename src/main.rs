6e3f203e17b24f69dd47eb440e110cf186e27f6f
tuorai <tubackkhoa@gmail.com>
2021-08-29 10:42:30 +0700

    remove server

--- src/main.rs
+++ src/main.rs
@@ -51,29 +51,6 @@ impl Config {
 }
 
 
-// empty struct
-pub struct CORS;
-
-impl Fairing for CORS {
-    fn info(&self) -> Info {
-        Info {
-            name: "Add CORS headers to requests",
-            kind: Kind::Response,
-        }
-    }
-
-    fn on_response(&self, _request: &Request, response: &mut Response) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new(
-            "Access-Control-Allow-Methods",
-            "POST, GET, OPTIONS",
-        ));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
-    }
-}
-
-
 fn query_wasm(request: &WasmQuery) -> QuerierResult {
     unsafe {
         let Config { engines, .. } = Config::get();
