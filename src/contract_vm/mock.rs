46a41ae91f3a4812627842914ca3b16ea0b9c336
tuorai <tubackkhoa@gmail.com>
2021-09-13 10:11:56 +0700

    remove ureq, left custom query unimplemented for future

--- src/contract_vm/mock.rs
+++ src/contract_vm/mock.rs
@@ -15,7 +15,6 @@ use cosmwasm_vm::{Backend, BackendError, BackendResult, GasInfo, Querier, Storag
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use std::collections::{BTreeMap, HashMap};
-use std::str::FromStr;
 
 use crate::contract_vm::querier::{CustomHandler, StdMockQuerier, WasmHandler};
 use crate::contract_vm::watcher;
@@ -134,66 +133,15 @@ impl MockQuerier {
 #[serde(rename_all = "snake_case")]
 /// An implementation of QueryRequest::Custom to show this works and can be extended in the contract
 pub enum SpecialQuery {
-    Fetch {
-        url: String,
-        method: Option<String>,
-        body: Option<String>,
-        headers: Option<Vec<String>>,
-    },
-}
-
-fn fetch(
-    url: &String,
-    method: &Option<String>,
-    body: &Option<String>,
-    headers: &Option<Vec<String>>,
-) -> MockQuerierCustomHandlerResult {
-    let mut req = match method {
-        Some(v) => ureq::request(v, url),
-        None => ureq::get(url),
-    };
-
-    // borrow ref
-    if headers.is_some() {
-        for line in headers.as_ref().unwrap() {
-            // if can parse Header
-            if let Ok(header) = ureq::Header::from_str(line) {
-                req = req.set(header.name(), header.value().unwrap_or_default());
-            }
-        }
-    }
-
-    let resp = match body {
-        Some(v) => req.send_string(v),
-        None => req.call(),
-    };
-
-    match resp {
-        Ok(response) => {
-            // return contract result
-            let input = response.into_string().unwrap_or_default();
-            // smart contract use base64 to decode bytes into structure
-            let result = base64::encode(input.as_bytes());
-            SystemResult::Ok(to_binary(&result).into())
-        }
-        Err(err) => SystemResult::Err(SystemError::InvalidRequest {
-            error: err.to_string(),
-            request: Binary::from(url.as_bytes()),
-        }),
-    }
+    // define custom query here for blockchain extension
 }
 
 impl CustomQuery for SpecialQuery {}
 
-pub fn custom_query_execute(query: &SpecialQuery) -> MockQuerierCustomHandlerResult {
-    match query {
-        SpecialQuery::Fetch {
-            url,
-            method,
-            body,
-            headers,
-        } => fetch(url, method, body, headers),
-    }
+pub fn custom_query_execute(_query: &SpecialQuery) -> MockQuerierCustomHandlerResult {
+    SystemResult::Err(SystemError::UnsupportedRequest {
+        kind: "Not implemented".to_string(),
+    })
 }
 
 #[derive(Default, Debug, Clone)]
